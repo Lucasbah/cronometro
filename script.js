5565e43404f6895b9de66611516ae13c4fbc71f7
LucasBah <lucaskeitaro@hotmail.com>
2023-09-26 19:43:40 +0000

    Prmeiro Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const minutesEl = document.querySelector("#minutes")
+const secondsEl = document.querySelector("#seconds")
+const milisecondsEl = document.querySelector("#miliseconds")
+const startBtn = document.querySelector("#startBtn")
+const pauseBtn = document.querySelector("#pauseBtn")
+const resumeBtn = document.querySelector("#resumeBtn")
+const resetBtn = document.querySelector("#resetBtn")
+
+let minutes = 0;
+let seconds = 0;
+let milliseconds = 0;
+let isPaused = false;
